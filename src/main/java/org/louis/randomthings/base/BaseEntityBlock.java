d3a848378936e243f720cef39db8c9b7e5b1d5e1
Louis <pn163163@gmail.com>
2025-02-01 18:47:19 +0700

    update base registry

---
+++ src/main/java/org/louis/randomthings/base/BaseEntityBlock.java
@@ -0,0 +1,51 @@
+package org.louis.randomthings.base;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public abstract class BaseEntityBlock extends BaseBlock implements EntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    public BaseEntityBlock(Function<Properties, Properties> properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction direction = context.getHorizontalDirection().getOpposite();
+        return this.defaultBlockState().setValue(FACING, direction);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, net.minecraft.world.level.block.Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, net.minecraft.world.level.block.Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public abstract BlockEntity newBlockEntity(BlockPos pos, BlockState state);
+}
---
+++ src/main/java/org/louis/randomthings/base/BaseEntityWaterloggedBlock.java
@@ -0,0 +1,59 @@
+package org.louis.randomthings.base;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public abstract class BaseEntityWaterloggedBlock extends BaseEntityBlock implements SimpleWaterloggedBlock {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+    public BaseEntityWaterloggedBlock(Function<Properties, Properties> properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(WATERLOGGED, Boolean.FALSE));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction direction = context.getHorizontalDirection().getOpposite();
+        FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
+        return this.defaultBlockState()
+                .setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : Fluids.EMPTY.defaultFluidState();
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(WATERLOGGED); // Thêm thuộc tính WATERLOGGED vào BlockState
+    }
+
+    @Override
+    public void onPlace(BlockState state, Level pLevel, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (state.getValue(WATERLOGGED)) {
+            pLevel.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+        }
+        super.onPlace(state, pLevel, pos, oldState, isMoving);
+    }
+
+    @Nullable
+    @Override
+    public abstract BlockEntity newBlockEntity(BlockPos pos, BlockState state);
+}
